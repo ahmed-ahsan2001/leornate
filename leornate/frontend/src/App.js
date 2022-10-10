9e29a57559e7147fa8588d965f68c56cfe4ce939
ahmed <ahmed-ahsan70@hotmail.com>
2022-10-10 23:54:48 +0500

    video3

---
+++ leornate/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Le-ORNATE</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
